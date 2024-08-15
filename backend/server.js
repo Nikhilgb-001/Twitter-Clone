160034c480026699c7bd220719f5d2fcbf6b4911
Nikhil <nikhilgb3113@gmail.com>
2024-08-15 15:01:48 +0530

    added user loginlogout signup and getting auth user fuctionality

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
+// main-file
 import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.js";
 import { connectDB } from "./db/connection.js";
 
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); //middleware for parsing req.body the form data coming from the react application
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 console.log(process.env.MONGO_URI);
 
