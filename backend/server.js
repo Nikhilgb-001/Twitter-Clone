46998dc45308e4868498537216d157b9dba2a5fc
Nikhil <nikhilgb3113@gmail.com>
2024-08-18 13:24:54 +0530

    completd building post model and controller

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,37 @@
 // main-file
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js";
+import postRoutes from "./routes/post.js";
+
 import { connectDB } from "./db/connection.js";
 
-import cookieParser from "cookie-parser";
-
 dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); //middleware for parsing req.body the form data coming from the react application
+//middleware for parsing req.body the form data coming from the react application
+app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 console.log(process.env.MONGO_URI);
 
+// routes
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
